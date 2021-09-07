5d1c7b8df7ece9905417e3505e73bfc4462aafdb
karamaru <38310693+karamaru-alpha@users.noreply.github.com>
2021-09-08 00:51:13 +0900

    マッチング開始のエンドポイント実装 (#29)
    * proto定義
    
    * Makefile定義
    
    * Makefile定義
    
    * protoc server
    
    * protoc client
    
    * add test

---
+++ server/test/match/match_test.go
@@ -0,0 +1,44 @@
+package api_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	pb "github.com/CA22-game-creators/cookingbomb-proto/server/pb/match"
+	"github.com/CA22-game-creators/cookingbomb-proto/server/validation"
+)
+
+func TestSignupRequest(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		title    string
+		input    *pb.MatchRequest
+		expected error
+	}{
+		{
+			title:    "【正常系】マッチング開始",
+			input:    &pb.MatchRequest{UserId: "00000000000000000000000001"},
+			expected: nil,
+		},
+		{
+			title:    "【正常系】不正なUserID",
+			input:    &pb.MatchRequest{UserId: "1"},
+			expected: errors.New("ユーザーIDが不正な形式です"),
+		},
+	}
+
+	for _, td := range tests {
+		td := td
+
+		t.Run("MatchRequest:"+td.title, func(t *testing.T) {
+			t.Parallel()
+
+			output := validation.Validate(td.input)
+
+			assert.Equal(t, td.expected, output)
+		})
+	}
+}
