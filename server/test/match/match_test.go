7420f6e7b6e817b9865fb0bca60148bd98a31ce6
Karacky <mrnk3078@gmail.com>
2021-10-23 16:28:37 +0900

    add auth match

--- server/test/match/match_test.go
+++ server/test/match/match_test.go
@@ -19,14 +19,28 @@ func TestSignupRequest(t *testing.T) {
 		expected error
 	}{
 		{
-			title:    "【正常系】マッチング開始",
-			input:    &pb.MatchRequest{UserId: "00000000000000000000000001"},
+			title: "【正常系】マッチング開始",
+			input: &pb.MatchRequest{
+				UserId:       "00000000000000000000000001",
+				SessionToken: "00000000-0000-0000-0000-000000000001",
+			},
 			expected: nil,
 		},
 		{
-			title:    "【正常系】不正なUserID",
-			input:    &pb.MatchRequest{UserId: "1"},
+			title: "【異常系】不正なUserID",
+			input: &pb.MatchRequest{
+				UserId:       "1",
+				SessionToken: "00000000-0000-0000-0000-000000000001",
+			},
 			expected: errors.New("ユーザーIDが不正な形式です"),
+		},
+		{
+			title: "【異常系】不正なSessionToken",
+			input: &pb.MatchRequest{
+				UserId:       "00000000000000000000000001",
+				SessionToken: "1",
+			},
+			expected: errors.New("sessionTokenが不正な形式です"),
 		},
 	}
 
